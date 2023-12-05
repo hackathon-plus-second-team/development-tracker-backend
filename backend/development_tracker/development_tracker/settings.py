51eb1de2f0854d3b812fcf8949d9b3d333254b91
Ольга Мелихова <melihova.o@yandex.ru>
2023-12-05 14:57:49 +0300

    fix: CORS_ALLOWED_ORIGINS port

--- backend/development_tracker/development_tracker/settings.py
+++ backend/development_tracker/development_tracker/settings.py
@@ -131,7 +131,7 @@ AUTH_USER_MODEL = "users.User"
 
 CORS_URLS_REGEX = r"^/api/.*$"
 
-CORS_ALLOWED_ORIGINS = ("http://localhost:3000",)
+CORS_ALLOWED_ORIGINS = ("http://localhost:5137",)
 
 if DEBUG:
     INSTALLED_APPS.extend(("drf_spectacular",))
